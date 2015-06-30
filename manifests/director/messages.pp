f93d63fb15b03397cb4be2542d88d947c26daa92
Simon Vigneux <svigneux@kronostechnologies.com>
2015-06-30 08:09:27 -0400

    adding more options to bareos::director::messages

--- manifests/director/messages.pp
+++ manifests/director/messages.pp
@@ -2,11 +2,29 @@
 #
 # Used to create messages resources
 #
+# Valid parameters are:
+#
+# $mail_command     - path to the smtp command used to send email
+# $mail_from        - email address used in the from: field
+# $mail_to          - email address used in the to: field
+# $mail_host        - smtp server used to send the email
+# $mail_type        - message type to send by email
+# $mailonerror_type - message type sent on job error
+# $console          - message type sent to the bareos console
+# $catalog          - message type sent to the catalog database
+# $append           - Append messages to a log file
+# $options_hash     - Extra configuration values
+#
 define bareos::director::messages (
   $mail_command = '',
-  $mail_host = 'localhost',
   $mail_from = '',
   $mail_to = '',
+  $mail_host = 'localhost',
+  $mail_type = 'all, !skipped',
+  $mailonerror_type = 'all',
+  $console = 'all, !skipped, !saved',
+  $catalog = 'all, !skipped, !saved',
+  $append = "\"${bareos::log_file}\" = all, !skipped",
   $options_hash = {},
   $template = 'bareos/director/messages.conf.erb'
 ) {
@@ -24,6 +42,14 @@ define bareos::director::messages (
       default => [$mail_to],
     },
     default   => $mail_to,
+  }
+
+  $array_append = is_array($append) ? {
+    false     => $append ? {
+      ''      => [],
+      default => [$append],
+    },
+    default   => $append,
   }
 
   $manage_messages_file_content = $template ? {
@@ -45,4 +71,3 @@ define bareos::director::messages (
   }
 
 }
-
