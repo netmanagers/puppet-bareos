2cc4603f5386efec87bd4ac50e706aefcda70a85
Jan Grewe <jan@faked.org>
2015-08-14 16:36:52 +0200

    Fix repository on Ubuntu

--- manifests/repository.pp
+++ manifests/repository.pp
@@ -19,14 +19,14 @@ class bareos::repository inherits bareos {
   if ($bool_manage_repository){
       case $::operatingsystem {
 
-        redhat,centos,fedora,Scientific,OracleLinux: {
+        /(?i:redhat|centos|fedora|scientific|oraclelinux)/: {
           file { 'bareos.repo':
             path    => '/etc/yum.repos.d/bareos.repo',
             content => template('bareos/bareos.repo.erb'),
           }
         }
 
-        Debian,Ubuntu: {
+        /(?i:debian|ubuntu)/: {
           file { '/etc/apt/sources.list.d/bareos.list':
             content => "deb http://download.bareos.org/bareos/release/${bareos::repo_flavour}/${bareos::repo_distro} /\n"
           }
